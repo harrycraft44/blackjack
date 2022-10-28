a0282a9efe452e4607268322d5a7fff99cdef82d
0xtk7 <harrisonelvin01@gmail.com>
2022-10-28 01:37:16 +0100

    added dealer and player cards

--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,7 @@ namespace blackjack
 {
     public partial class Form1 : Form
     {
-        struct card
+        public struct card
         {
             public int value;
             public string suit;
@@ -11,7 +11,10 @@ namespace blackjack
         int[] cardValue = { 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
         string[] suits = { "Diamond", "Spades", "Clubs", "Hearts" };
 
-        public void getCards()
+        card[] DealerCards = new card[10];
+        card[] PlayerCards = new card[10];
+
+        public card getCards()
         {
             card card = new card();
             Random rand = new Random();
@@ -21,6 +24,8 @@ namespace blackjack
 
             label1.Text = card.value.ToString();
             label2.Text = card.suit;
+
+            return card;
         }
 
         public Form1()
@@ -30,12 +35,17 @@ namespace blackjack
 
         private void Form1_Load(object sender, EventArgs e)
         {
-         
+            // add dealer's cards 
+            DealerCards[0] = getCards();
+            DealerCards[1] = getCards();
+
+            label1.Text = DealerCards[0].ToString();
+            label2.Text = DealerCards[1].ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            getCards();
+            
         }
     }
 }
