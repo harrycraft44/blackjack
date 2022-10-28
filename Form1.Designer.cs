4b841623443fe9f246446fa9f55734a99c3d36b3
0xtk7 <harrisonelvin01@gmail.com>
2022-10-28 01:27:05 +0100

    added suit and cards

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,33 +29,60 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(851, 519);
+            this.button1.Location = new System.Drawing.Point(596, 311);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(253, 44);
+            this.button1.Size = new System.Drawing.Size(177, 26);
             this.button1.TabIndex = 0;
             this.button1.Text = "hit";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(461, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(476, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1208, 689);
+            this.ClientSize = new System.Drawing.Size(846, 413);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button button1;
+        private Label label1;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,14 +2,26 @@ namespace blackjack
 {
     public partial class Form1 : Form
     {
-
         struct card
         {
-            string name;
-            string suit;
-            int value; // Value of the card number 2-11
+            public int value;
+            public string suit;
+        }
 
-        };
+        int[] cardValue = { 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
+        string[] suits = { "Diamond", "Spades", "Clubs", "Hearts" };
+
+        public void getCards()
+        {
+            card card = new card();
+            Random rand = new Random();
+
+            card.value = cardValue[rand.Next(13)];
+            card.suit = suits[rand.Next(4)];
+
+            label1.Text = card.value.ToString();
+            label2.Text = card.suit;
+        }
 
         public Form1()
         {
@@ -18,15 +30,12 @@ namespace blackjack
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+         
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            Random rnd = new Random();
-            int card = rnd.Next(52);
-
+            getCards();
         }
     }
 }
